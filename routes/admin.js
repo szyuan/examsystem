8ea1545444843f7f70272785f2b7d7d1e53f98eb
szy <466793240@qq.com>
2016-04-19 02:06:56 +0800

    管理页面-新建考试

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('admin-index');
+});
 router.get('/main', function(req, res, next) {
   res.render('admin-index');
 });
