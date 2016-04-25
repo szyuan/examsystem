a223c628a5f99b6cdd61bd0781aaf1e5d4c8c065
szyuan <shangzy@turingcat.com>
2016-04-25 18:32:36 +0800

    添加选项部分

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -7,9 +7,16 @@ $(function(){
 		var questionList=$('.question-list');
 		var questionFormWrap=$('.question-form-wrap').eq(0);
 		var addQuestionBtn=$('.add-question-btn');
+		var addOptionBtn=$('.add-option-btn');
+
 		addQuestionBtn.click(function(){
 			var qfwClone=questionFormWrap.clone(true);
 			questionList.append(qfwClone);
+		});
+
+		addOptionBtn.click(function(e) {
+			var optionItem=$(e.target).parent().siblings('.option-list');
+			console.log(optionItem);
 		});
 
 	})();
